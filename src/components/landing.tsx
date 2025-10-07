adcde7cbadd2eb43ea5aae95590fc71498a13a5f
GIT-Saikat <saikatmondal2105@gmail.com>
2025-10-07 12:51:24 +0530

    changing machines ..so last update on landing page

---
+++ src/components/landing.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div className="flex justify-between">
+      <div className="min h-screen">
+        {/* LEft */}
+        <div>Create Colllaborat visulayy</div>
+        <div>
+          Excalidraw is a virtual collaborative whiteboard tool that lets you
+          easily sketch diagrams that have a hand-drawn feel to them
+        </div>
+        <div>Button get started for free</div>
+        <div>Button signin</div>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
